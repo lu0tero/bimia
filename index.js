3c8417cf9043518d1116b58de28301eaf1f7212f
lu0tero <luotero.99@gmail.com>
2023-06-28 16:46:37 +0100

    cookies

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+// Burger Menu
 const burgerMenu = document.getElementById('burger-menu');
 const header = document.getElementById('header');
 
 burgerMenu.addEventListener('click', ()=> {
    header.classList.toggle('isActive')
 
+})
+
+// Cookies
+const cookiesBtn = document.getElementById('cookies-btn');
+const cookiesCard = document.getElementById('cookies-card');
+const cookiesBackground = document.getElementById('cookies-background');
+
+if(!localStorage.getItem('agree-cookies')) {
+   cookiesCard.classList.add('active');
+   cookiesBackground.classList.add('active');
+}
+
+
+cookiesBtn.addEventListener('click', () => {
+   cookiesCard.classList.remove('active');
+   cookiesBackground.classList.remove('active');
+
+   localStorage.setItem('agree-cookies', true)
 })
